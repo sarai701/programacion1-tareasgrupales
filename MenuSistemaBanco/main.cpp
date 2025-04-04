eaa14b95ae6e68dc92529387271fd63e794f6b4a
Sarai Calgua <sarai.calgua701@gmaol.com>
2025-04-03 22:01:34 -0600

    modificacion de main y banco

--- MenuSistemaBanco/main.cpp
+++ MenuSistemaBanco/main.cpp
@@ -1,1 +1,11 @@
+//Priscila Sarai Guzmán Calgua
+#include "Banco.h"
 
+
+//Función principal del sistema.
+//Crea una instancia de Banco y ejecuta el menú.
+int main() {
+    Banco banco;
+    banco.mostrarMenu();
+    return 0;
+}
