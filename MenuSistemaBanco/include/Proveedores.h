60ed0089bd0795bf1fc4f0cbfc678f6d635dd323
Daniela Divas <ddanielanohemy@gmail.com>
2025-04-03 23:27:21 -0600

    proveedoressss

--- MenuSistemaBanco/include/Proveedores.h
+++ MenuSistemaBanco/include/Proveedores.h
@@ -1,1 +1,16 @@
+//Daniela Nohemy Divas Carrillo
+#ifndef PROVEEDOR_H
+#define PROVEEDOR_H
+
+#include <iostream>
+
+//Clase Proveedor
+//Maneja el registro de facturas de proveedores.
+class Proveedor {
+public:
+    //Simula el registro de una factura de un proveedor.
+    void registrarFactura();
+};
+
+#endif
 
