30a0acd729400325310017778dd6af7899ad14c0
Jonathan Samuel Gonzalez <jgonzalezi7@miumg.edu.gt>
2025-04-03 22:11:38 -0600

    subida de empleados

--- MenuSistemaBanco/src/Empleados.cpp
+++ MenuSistemaBanco/src/Empleados.cpp
@@ -1,1 +1,14 @@
 
+#include "Empleados.h"
+
+using namespace std;
+
+//Simula la gestión de nóminas de empleados.
+void Empleado::gestionarNomina() {
+    cout << "Gestionando nomina de empleados...\n";
+    cout << "Presione ENTER para volver al menú principal...";
+    cin.ignore();
+    cin.get();
+    system("cls");
+
+}
