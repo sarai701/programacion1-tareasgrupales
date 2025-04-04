2be0f6b088ec19f69e73fec55b6d1110baa53eb0
Ivan774gt <deleonboris6@gmail.com>
2025-04-03 23:58:31 -0600

    Se agregaron clientes

--- MenuSistemaBanco/include/Cliente.h
+++ MenuSistemaBanco/include/Cliente.h
@@ -1,1 +1,20 @@
+//Karla Patricia Ruiz Ordoñez
+#ifndef CLIENTE_H
+#define CLIENTE_H
 
+#include <iostream>
+
+//Clase Cliente
+//Maneja los pagos y la consulta de saldo de clientes.
+
+class Cliente {
+public:
+    //Simula la realización de un pago de servicios.
+
+    void realizarPago();
+
+    //*Simula la consulta de saldo y movimientos de un cliente.
+    void consultarSaldo();
+};
+
+#endif
