56bdf3bc145b272689b537d6d7b026241cf63410
KarlaRuiz8 <kruizo@miumg.edu.gt>
2025-04-03 22:35:54 -0600

    modificacion de clientes

--- MenuSistemaBanco/include/Cliente.h
+++ MenuSistemaBanco/include/Cliente.h
@@ -1,1 +1,19 @@
+#ifndef CLIENTE_H
+#define CLIENTE_H
 
+#include <iostream>
+
+//Clase Cliente
+//Maneja los pagos y la consulta de saldo de clientes.
+
+class Cliente {
+public:
+    //Simula la realización de un pago de servicios.
+
+    void realizarPago();
+
+    //*Simula la consulta de saldo y movimientos de un cliente.
+    void consultarSaldo();
+};
+
+#endif
--- MenuSistemaBanco/src/Cliente.cpp
+++ MenuSistemaBanco/src/Cliente.cpp
@@ -1,1 +1,24 @@
+#include "Cliente.h"
+using namespace std;
 
+
+//Simula la realización de un pago de servicios.
+void Cliente::realizarPago() {
+    cout << "Realizando pago de servicios...\n";
+    cout << "Presione ENTER para volver al menú principal...";
+    cin.ignore();
+    cin.get();
+    system("cls");
+}
+
+//Simula la consulta de saldo y movimientos de un cliente.
+
+void Cliente::consultarSaldo() {
+    cout << "Consultando saldo y movimientos...\n";
+    cout << "Presione ENTER para volver al menú principal...";
+    cin.ignore();
+    cin.get();
+    system("cls");
+
+
+}
